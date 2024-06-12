d37d334ec980638bcdd60e993ba9ec4e48691cd7
Gabo_Tech <gabriel@beyondbmi.ie>
2024-06-13 01:26:40 +0200

    First components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { SongsListComponent } from './components/songs-list/songs-list.component';
+import { SongDetailComponent } from './components/song-detail/song-detail.component';
+import { SongFormComponent } from './components/song-form/song-form.component';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
 }
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SongsListComponent, SongDetailComponent, SongFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
